2114148b676671cf37d6f7b5a527ef62e90d1200
User <kaungmyatsoe.m192@gmail.com>
2025-07-27 22:05:17 +0630

    fix: background

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,25 +1,45 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <style>
+        .login-image {
+            background-image: url('backend/media/budget-tracker-bg.jpg');
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+</head>
+
+<body class="font-sans text-gray-900 antialiased">
+    <div class="flex">
+        <div class="flex-1 hidden lg:block">
+            <img src="backend/media/budget-tracker-bg.jpg" alt="" class="w-full min-h-screen">
+            {{-- <div class="flex items-center justify-center min-h-screen">
+                <div class="text-center p-8 text-white"> 
+                    <h1 class="text-6xl font-black mb-4">{{ env('APP_NAME') }}</h1>
+                    <p class="text-xl">Track your income and expenses effortlessly</p>
+                 </div>
+            </div>  --}}
+        </div>
+        <div class="flex-1 min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
                 <a href="/">
                     {{-- <x-application-logo class="w-20 h-20 fill-current text-gray-500" /> --}}
-                    {{ env("APP_NAME") }}
+                    {{ env('APP_NAME') }}
                 </a>
             </div>
 
@@ -27,5 +47,8 @@
                 {{ $slot }}
             </div>
         </div>
-    </body>
+    </div>
+
+</body>
+
 </html>
